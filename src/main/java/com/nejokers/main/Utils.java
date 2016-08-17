cfd9ff2cad618297df6cb97fd1a8f70a80de8442
herbert scruggs <herbscruggs@gmail.com>
2016-08-17 15:25:16 -0500

    changes for Demo

--- src/main/java/com/nejokers/main/Utils.java
+++ src/main/java/com/nejokers/main/Utils.java
@@ -1,6 +1,8 @@
 package com.nejokers.main;
 
 import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,65 +10,86 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.ITestContext;
 
 public class Utils {
 
-	public static String browserType;
+    public static String browserType;
 
-	
-	public static WebDriver getDriver(ITestContext context) {
-//Good Start
-	       File file;
-	       WebDriver driver = null;
 
-	       switch (context.getCurrentXmlTest().getParameter("browse")) {
-	           case "CHROME":
-	               file = new File(
-	                   context.getCurrentXmlTest().getParameter("chromedriver"));
-	               System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
-	               driver = new ChromeDriver();
-	               break;
-	           case "IE":
-	               file = new File(
-	                   context.getCurrentXmlTest().getParameter("iedriver"));
-	               System.setProperty("webdriver.ie.driver", file.getAbsolutePath());
-	               DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
-	               capabilities.setCapability("nativeEvents", true);
-	               capabilities.setCapability("ignoreProtectedModeSettings", true);
-	               capabilities.setCapability("requireWindowFocus", true);
-	               capabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
-	               capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
-	               driver = new InternetExplorerDriver(capabilities);
-	               break;
-	           default:
-	               driver = new FirefoxDriver();
-	               break;
-	       }
+    public static WebDriver getDriver(ITestContext context) {
+        //Good Start
+        File file;
+        WebDriver driver = null;
 
-	       setBrowserType(context.getCurrentXmlTest().getParameter("browse"));
-	       driver.manage().window().maximize();
-	       return driver;
-	   }
+        switch (context.getCurrentXmlTest().getParameter("browse")) {
+            case "CHROME":
+                file = new File(
+                    context.getCurrentXmlTest().getParameter("chromedriver"));
+                System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+                driver = new ChromeDriver();
+                break;
+            case "IE":
+                file = new File(
+                    context.getCurrentXmlTest().getParameter("iedriver"));
+                System.setProperty("webdriver.ie.driver", file.getAbsolutePath());
+                DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
+                capabilities.setCapability("nativeEvents", true);
+                capabilities.setCapability("ignoreProtectedModeSettings", true);
+                capabilities.setCapability("requireWindowFocus", true);
+                capabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+                capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+                driver = new InternetExplorerDriver(capabilities);
+                break;
+            case "HUBCHROME":
+                try {
+                    driver = new RemoteWebDriver(
+                        new URL(context.getCurrentXmlTest().getParameter("docker_address")),
+                        DesiredCapabilities.chrome());
+                } catch (MalformedURLException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                break;
+            case "HUBFIREFOX":
+                try {
+                    driver = new RemoteWebDriver(
+                        new URL(context.getCurrentXmlTest().getParameter("docker_address")),
+                        DesiredCapabilities.firefox());
+                } catch (MalformedURLException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                break;
 
-	public static String getBrowserType() {
-	       return browserType;
-	   }
+            default:
+                driver = new FirefoxDriver();
+                break;
+        }
 
-	   public static void setBrowserType(String appType) {
-	       browserType = appType;
-	   }
-	
-	   public static void sleep(int value)
-	   {
-			try {
-			Thread.sleep(value);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	   
-      }
-	   
-	   
+        setBrowserType(context.getCurrentXmlTest().getParameter("browse"));
+        driver.manage().window().maximize();
+        return driver;
+    }
+
+    public static String getBrowserType() {
+        return browserType;
+    }
+
+    public static void setBrowserType(String appType) {
+        browserType = appType;
+    }
+
+    public static void sleep(int value) {
+        try {
+            Thread.sleep(value);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+
 }
